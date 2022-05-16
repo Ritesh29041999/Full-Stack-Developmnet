2c60cbbfba6de9ff034519cf8b9976f0cefe6575
Ritesh29041999 <102539185+Ritesh29041999@users.noreply.github.com>
2022-05-16 18:20:47 +0530

    try

---
+++ src/ArithmeticOperators.java
@@ -0,0 +1,12 @@
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		System.out.println("Hello India");
+		System.out.println("Edit");
+		System.out.println("Github");
+				
+
+	}
+
+}
